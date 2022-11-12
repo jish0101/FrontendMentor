4adca23af37192ec3758f0c0c43ade3f6096ba01
Hero <76834176+jish0101@users.noreply.github.com>
2022-11-12 19:23:00 +0530

    done.!

--- manage-landing-page-master/js/script.js
+++ manage-landing-page-master/js/script.js
@@ -1,9 +1,27 @@
-const navToggle = document.querySelector('.mobile-nav-toggle');
-const primaryNav = document.querySelector('.primary-navigation');
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
 
-navToggle.addEventListener('click', () => {
-    primaryNav.hasAttribute('data-visible')
-        ? navToggle.setAttribute('aria-expanded', false)
-        : navToggle.setAttribute('aria-expanded', true);
-    primaryNav.toggleAttribute('data-visible');
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  autoplay: true,
+  centerMode: true,
+  dots: true,
+  slidesToShow: 1,
+  arrows: false,
+  autoplaySpeed: 2500,
+  responsive: {
+    600: {
+      dots: false,
+      slidesToShow: 3,
+    },
+  },
 });
